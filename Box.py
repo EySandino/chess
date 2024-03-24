dcb23dc76a810cff73d258717767c60b8d0e9d10
Eyder <eyder.sandino@gmail.com>
2024-03-24 17:20:35 -0600

    empieza la creacion de los movimientos

--- Box.py
+++ Box.py
@@ -3,14 +3,23 @@ from Images import imgWhiteBox, imgBlackBox
 from Pieces import *
 
 class Box():
-    img = None
 
     def __init__(self, boardFrame, boxColor, boardRow, boardColumn):
+        #Generate the board box label object
         self.generateBox(boardFrame)
+
+        #Set the board x, y coordinates
         self.setPosition(boardRow, boardColumn)
-        self.setColor(boxColor)
+
+        #Set color value variable and color of the box
+        self.setBoxColorValue(boxColor)
+        self.setColor(self.boxColor)
+
+        #Create the piece object if needed
         self.setPiece()
-        self.setBoxPhoto(boxColor)
+        self.setBoxPhoto(self.boxColor)
+
+        #Set the label in the boardFrame grid
         self.placeBox()
 
     #Get box properties
@@ -22,61 +31,79 @@ class Box():
         self.boardBox.config(cursor=cursorInput)
 
     def setBoxPhoto(self, boxColor):
-        global img
-
         if (self.piece != None):
-            img = PhotoImage(file=self.piece.getPieceImage())
-            self.boardBox.config(image=img)
+            self.img = PhotoImage(file=self.piece.getPieceImage())
+            self.boardBox.config(image=self.img)
         else:
             if (boxColor == "#FFFFFF"):
-                img = PhotoImage(file=imgWhiteBox)
-                self.boardBox.config(image=img)
+                self.img = PhotoImage(file=imgWhiteBox)
+                self.boardBox.config(image=self.img)
             elif (boxColor == "#76ABAE"):
-                img = PhotoImage(file=imgBlackBox)
-                self.boardBox.config(image=img)
+                self.img = PhotoImage(file=imgBlackBox)
+                self.boardBox.config(image=self.img)
 
     def setPosition(self, x, y):
         self.boxPosition = (x, y)
+
+    def setBoxColorValue(self, colorValue):
+        self.boxColor = colorValue
     
     def setColor(self, boxColor):
         self.boardBox.config(bg=boxColor)
 
     def setPiece(self):
         #Generate the "piece" object that will occupy the box
-        if (self.boxPosition[0] not in range(2, 6)):
-            self.setCursor("hand2")
+        if (self.getPosition()[0] not in range(2, 6)):
+            self.enableTriggerAction()
 
             #Define Pawn
-            if ((self.boxPosition[0] == 1) or (self.boxPosition[0] == 6)):
+            if ((self.getPosition()[0] == 1) or (self.getPosition()[0] == 6)):
                 self.piece = Pawn(self.getPosition())
 
             #Define Rook
-            elif ((self.boxPosition[1] == 0) or (self.boxPosition[1] == 7)):
+            elif ((self.getPosition()[1] == 0) or (self.getPosition()[1] == 7)):
                 self.piece = Rook(self.getPosition())
 
             #Define Knight
-            elif ((self.boxPosition[1] == 1) or (self.boxPosition[1] == 6)):
+            elif ((self.getPosition()[1] == 1) or (self.getPosition()[1] == 6)):
                 self.piece = Knight(self.getPosition())
 
             #Define Bishop
-            elif ((self.boxPosition[1] == 2) or (self.boxPosition[1] == 5)):
+            elif ((self.getPosition()[1] == 2) or (self.getPosition()[1] == 5)):
                 self.piece = Bishop(self.getPosition())
 
             #Define Queen
-            elif ((self.boxPosition[1] == 3)):
+            elif ((self.getPosition()[1] == 3)):
                 self.piece = Queen(self.getPosition())
 
             #Define King
-            elif ((self.boxPosition[1] == 4)):
+            elif ((self.getPosition()[1] == 4)):
                 self.piece = King(self.getPosition())
         #If the box is empty the object won't be generated
         else:
             self.setCursor("arrow")
             self.piece = None
-    
+
+    def movePiece(self, event):
+        self.setColor("#FFC700")
+        self.piece.displayMoveSet()
+
+    def doNothing(self, event):
+        pass
+
+
     #Generate methods
     def generateBox(self, boardFrame):
         self.boardBox = Label(boardFrame, width=48, height=48)
 
     def placeBox(self):
-        self.boardBox.grid(row=self.boxPosition[0], column=self.boxPosition[1])
+        self.boardBox.grid(row=self.getPosition()[0], column=self.getPosition()[1])
+        
+    def enableTriggerAction(self):
+        self.setCursor("hand2")
+        self.boardBox.bind("<Button-1>", self.movePiece)
+
+    def disableTriggerAction(self):
+        self.setCursor("arrow")
+        self.setColor(self.boxColor)
+        self.boardBox.bind("<Button-1>", self.doNothing)
--- Pieces.py
+++ Pieces.py
@@ -1,4 +1,5 @@
 from Images import *
+from BoardCoordinates import boardCoordinatesList
 
 class Piece():
 
@@ -10,25 +11,31 @@ class Piece():
     def setPosition(self, boxPosition):
         self.piecePosition = boxPosition
 
+    def getPosition(self):
+        return self.piecePosition
+
     def getPieceImage(self):
         return self.pieceImage
 
     #Overridable methods
     def setColor(self):
-        if (self.piecePosition[0] == 0):
+        if (self.getPosition()[0] == 0):
             self.color = "black"
-        elif (self.piecePosition[0] == 7):
+        elif (self.getPosition()[0] == 7):
             self.color = "white"
     
     def setPieceImage(self):
         pass
 
+    def displayMoveSet(self):
+        pass
+
 class Pawn(Piece):
 
     def setColor(self):
-        if (self.piecePosition[0] == 1):
+        if (self.getPosition()[0] == 1):
             self.color = "black"
-        elif (self.piecePosition[0] == 6):
+        elif (self.getPosition()[0] == 6):
             self.color = "white"
 
     def setPieceImage(self):
@@ -36,6 +43,39 @@ class Pawn(Piece):
             self.pieceImage = imgBlackPawn
         elif (self.color == "white"):
             self.pieceImage = imgWhitePawn
+
+    def displayMoveSet(self):
+        #Pawns can only move one box forward
+        #It captures one box diagonal
+        #If the pawn is in its initial row, it can move two boxes forward
+        #Pawns can do "en passant capture" if the enemy pawn next to them just moved from its starting box
+
+        self.moveSetList = []
+
+        #Current Position
+        x = self.getPosition()[0]
+        y = self.getPosition()[1]
+
+        moveFactor = 0
+        if (self.color == "white"):
+            moveFactor = -1
+        elif (self.color == "black"):
+            moveFactor = 1
+
+        #Move ahead
+        if (boardCoordinatesList[x + moveFactor][y].piece == None):
+            self.moveSetList.append((x + moveFactor, y))
+
+            if (((x == 1) and (self.color == "black")) or ((x == 6) and (self.color == "white"))):
+                self.moveSetList.append((x + (moveFactor * 2), y))
+        
+        #Regular Capure
+
+        #En passant capture
+
+        #Apply color changes to available boxes
+        for position in self.moveSetList:
+            boardCoordinatesList[position[0]][position[1]].setColor("#FFF455")
         
 class Rook(Piece):
 
@@ -45,6 +85,9 @@ class Rook(Piece):
         elif (self.color == "white"):
             self.pieceImage = imgWhiteRook
 
+    def displayMoveSet(self):
+        pass
+
 class Knight(Piece):
 
     def setPieceImage(self):
@@ -52,6 +95,9 @@ class Knight(Piece):
             self.pieceImage = imgBlackKnight
         elif (self.color == "white"):
             self.pieceImage = imgWhiteKnight
+
+    def displayMoveSet(self):
+        pass
 
 class Bishop(Piece):
 
@@ -69,6 +115,9 @@ class Queen(Piece):
         elif (self.color == "white"):
             self.pieceImage = imgWhiteQueen
 
+    def displayMoveSet(self):
+        pass
+
 class King(Piece):
 
     def setPieceImage(self):
@@ -76,3 +125,6 @@ class King(Piece):
             self.pieceImage = imgBlackKing
         elif (self.color == "white"):
             self.pieceImage = imgWhiteKing
+
+    def displayMoveSet(self):
+        pass
